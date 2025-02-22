310674fff37724ec91c0ec29792e9aab293c56a1
Tomo Wang <tomo_wang@163.com>
2025-02-22 11:43:55 +0800

    feat: C4-6 init doc - using ollama API in golang

---
+++ notebook/C4/Golang_API_example/ollama-demo/chat/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/ollama/ollama/api"
+)
+
+func main() {
+	client, err := api.ClientFromEnvironment()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	messages := []api.Message{
+		api.Message{
+			Role:    "user",
+			Content: "为什么天空是蓝色的？",
+		},
+	}
+
+	ctx := context.Background()
+	req := &api.ChatRequest{
+		Model:    "deepseek-r1:7b",
+		Messages: messages,
+	}
+
+	respFunc := func(resp api.ChatResponse) error {
+		fmt.Print(resp.Message.Content)
+		return nil
+	}
+
+	err = client.Chat(ctx, req, respFunc)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
